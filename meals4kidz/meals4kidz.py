e30f47428cb662a6a0ca4e149fa4e49c38ab6078
Terry Shim <terryshimbo@outlook.com>
2022-10-17 00:44:18 -0400

    Update meals4kidz.py
    commented code for clearer understanding

--- meals4kidz/meals4kidz.py
+++ meals4kidz/meals4kidz.py
@@ -19,23 +19,23 @@ testRecipe = {
         'Rice paper wraps': '1 carrot'
 }
 
-def checkValidMeal(meal):
+def checkValidMeal(meal): #checking that a meal cannot be a number
     if meal.isnumeric():
         return False
     else:
         return True
 
-def measurementList(recipes, meal):
+def measurementList(recipes, meal): #parse through a dictionary of recipes in order to find the measurements of the ingredients
     try:
         # Iterating over values
        
-        print('Measurements of: ' + str(meal) + '\n')
-        for meal, ingredient in recipes.items():   
-            assert checkValidMeal(meal) == True
+        print('Measurements of: ' + str(meal) + '\n') #specify which meal you are taking from
+        for meal, ingredient in recipes.items():   #iterate through the recipes to find the correct one 
+            assert checkValidMeal(meal) == True   #error checking
             
-            return print(ingredient)
+            return print(ingredient)     #print only the measurement list
     except:
-        raise Exception("Meal cannot be a number!")
+        raise Exception("Meal cannot be a number!")   #error if the meal is a number
         
 
 
